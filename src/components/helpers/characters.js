9bb45e8833f262dab57c537b0fe21012a041e6af
ritzenthaler.audrey@gmail.com <ritzenthaler.audrey@gmail.com>
2020-09-30 15:48:07 +0200

    create helpers with 2 functions who helps me to create random time and random character

---
+++ src/components/helpers/characters.js
@@ -0,0 +1,11 @@
+export const randomChar = () => {
+   const arrayChar = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '?', ':', '§', '%']
+   const randomNumber = Math.floor(Math.random() * arrayChar.length)
+   return arrayChar[randomNumber]
+}
+
+export const randomTime = () => {
+   const max = 3000
+   const min = 800
+   return Math.random() * (max - min) + min
+}
